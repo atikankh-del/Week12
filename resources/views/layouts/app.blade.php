ea358b64b7d89ce91378145c558629c7fd19908a
atikankh-del <atikan.kh@rmuti.ac.th>
2026-08-31 15:19:03 +0700

    Rename application to Blog Application

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -8,7 +8,7 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>@yield('title', config('app.name', 'Laravel Blog'))</title>
+    <title>@yield('title', config('app.name', 'Blog Application'))</title>
 
     <!-- Fonts -->
     <link rel="dns-prefetch" href="//fonts.bunny.net">
@@ -24,7 +24,7 @@
         <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
             <div class="container">
                 <a class="navbar-brand" href="{{ route('welcome') }}">
-                    {{ config('app.name', 'Laravel Blog') }}
+                    {{ config('app.name', 'Blog Application') }}
                 </a>
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
                     data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
